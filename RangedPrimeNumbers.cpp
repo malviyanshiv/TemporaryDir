b8172319304e5a5e14aab75e648ed24df331eb32
Salman Ansari <salman.ansari776@gmail.com>
2018-12-31 20:27:07 +0530

    Update RangedPrimeNumbers.cpp

--- RangedPrimeNumbers.cpp
+++ RangedPrimeNumbers.cpp
@@ -3,6 +3,8 @@
 https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-1/practice-problems/algorithm/hell-1/
 */
 
+//TLE after TestCase 53
+
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -11,9 +13,10 @@ int main() {
     int q;
     std::cin >> q;
    // cin>>MAX_N;
-    int MAX_N=1000002; 
+    int MAX_N=1000002;
     int root_max_n=1000;
-    bool prime_arr[MAX_N];// to point out which element is prime 
+    //int root_max_n=sqrt(MAX_N+1);
+    bool prime_arr[MAX_N];
     for(int i=0;i<MAX_N;i++)
     prime_arr[i]=true;
     
@@ -21,6 +24,7 @@ int main() {
     prime_arr[0]=false;
     prime_arr[1]=false;
     
+    //prime_arr[2]=true;
     for(int i=2;i<=root_max_n;i++)
     {
         if(prime_arr[i]==true)
@@ -30,15 +34,15 @@ int main() {
         }
     }
     
-    int cout_prime_arr[MAX_N];  // this array will hold the counts of prime numbers till ith number
-    int c=0;
-    cout_prime_arr[0]=cout_prime_arr[1]=0;
-    for(int i=0;i<MAX_N;i++)
-    {
-        if(prime_arr[i])
-        c++;
-        cout_prime_arr[i]=c;
-    }
+    int cout_prime_arr[MAX_N];
+    //int c=0;
+    //cout_prime_arr[0]=cout_prime_arr[1]=0;
+    // for(int i=0;i<MAX_N;i++)
+    // {
+    //     if(prime_arr[i])
+    //     c++;
+    //     cout_prime_arr[i]=c;
+    // }
     // for(int i=0;i<=MAX_N;i++)
     // cout<<prime_arr[i]<<" ";
     // cout<<endl;
@@ -49,7 +53,15 @@ int main() {
     {
         int n,m;
         cin>>n>>m;
-        cout<<abs(cout_prime_arr[m]-cout_prime_arr[n-1])<<endl;
+        int a=min(n,m);
+        int b=max(n,m);
+        int c=0;
+        for(int i=a;i<=b;i++)
+        {
+            if(prime_arr[i])
+            c++;
+        }
+        cout<<c<<endl;
     }
 	return 0;
 }
