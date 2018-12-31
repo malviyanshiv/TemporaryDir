3773a24c7dba397a4f3311a402ec54b0477de1b1
Salman Ansari <salman.ansari776@gmail.com>
2018-12-31 20:48:19 +0530

    added counting method

--- RangedPrimeNumbers.cpp
+++ RangedPrimeNumbers.cpp
@@ -35,32 +35,39 @@ int main() {
     }
     
     int cout_prime_arr[MAX_N];
-    //int c=0;
-    //cout_prime_arr[0]=cout_prime_arr[1]=0;
-    // for(int i=0;i<MAX_N;i++)
-    // {
-    //     if(prime_arr[i])
-    //     c++;
-    //     cout_prime_arr[i]=c;
-    // }
+    for(int i=0;i<MAX_N;i++)
+    {
+        if(prime_arr[i])
+        c++;
+        cout_prime_arr[i]=c;
+    }
+	
+	
     // for(int i=0;i<=MAX_N;i++)
     // cout<<prime_arr[i]<<" ";
     // cout<<endl;
     // for(int i=0;i<MAX_N;i++)
     // cout<<cout_prime_arr[i]<<" ";
-    //for(int i=)
     for(int t=1;t<=q;t++)
     {
         int n,m;
         cin>>n>>m;
-        int a=min(n,m);
-        int b=max(n,m);
+        int a=min(n,m);//here max/min is use to get rid of 9 7 from 7 9 because in both cases ans will vary according
+        int b=max(n,m);//to implementation
         int c=0;
         for(int i=a;i<=b;i++)
         {
             if(prime_arr[i])
             c++;
         }
+	    
+	    /*
+	    total counting approach
+	    int a=max(n,m);
+	    int b=min(n,m);
+	    int c=cout_prime_arr[a]-cout_prime_arr[b-1];
+	    */
+	    
         cout<<c<<endl;
     }
 	return 0;
